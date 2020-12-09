17701a9e0068a176053f0ae1b2749f28f2767e5b
Rick M <spookcity138@gmail.com>
2020-12-09 16:55:04 +0100

    guacamoleb: Switch back to OP BUILD_FINGERPRINT

--- evolution_guacamoleb.mk
+++ evolution_guacamoleb.mk
@@ -39,7 +39,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 PRODUCT_CHARACTERISTICS := nosdcard
 
 # Build info
-BUILD_FINGERPRINT := google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys
+BUILD_FINGERPRINT := "OnePlus/OnePlus7/OnePlus7:10/QKQ1.190716.003/2011052232:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=OnePlus7 \
     PRODUCT_NAME=OnePlus7 \
