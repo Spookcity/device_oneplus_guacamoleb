baeff517a8e63e6a2d5688fee0039d823e3c96de
AnierinB <anierin@evolution-x.org>
2022-10-21 22:18:06 +0000

    guacamoleb: Initialize Evolution X 7.x
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,14 @@
 #
 # Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2022 The Evolution X Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_guacamoleb.mk
+    $(LOCAL_DIR)/evolution_guacamoleb.mk
+
+COMMON_LUNCH_CHOICES := \
+    evolution_guacamoleb-user \
+    evolution_guacamoleb-userdebug \
+    evolution_guacamoleb-eng
--- device.mk
+++ device.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2018-2019 The LineageOS Project
+# Copyright (C) 2022 The Evolution X Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -14,19 +15,14 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_ODM)/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_ODM)/etc/sound_trigger_mixer_paths.xml
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2240
-TARGET_SCREEN_WIDTH := 1080
-
 # Device init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-
 PRODUCT_PACKAGES += \
+    EvolutionSettingsResTarget \
+    EvolutionSystemUIResTarget \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSystemUIResTarget
