ad5c45b3a97ed425e011430534cc2132df0d55cc
LuK1337 <priv.luk@gmail.com>
2022-11-03 10:59:05 +0100

    guacamoleb: overlay: Configure aux camera for Aperture

--- device.mk
+++ device.mk
@@ -21,6 +21,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 PRODUCT_PACKAGES += \
+    ApertureResTarget \
     EvolutionSettingsResTarget \
     EvolutionSystemUIResTarget \
     OPlusFrameworksResTarget \
